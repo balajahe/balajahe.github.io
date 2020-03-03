15e2fe6c2168d62c47c8f01d42db0b3dd1c2591a
balajahe <balajahe@gmail.com>
2020-03-03 20:39:38 +0300

    dev

--- cctv_mobile_pwa/video-detector.js
+++ cctv_mobile_pwa/video-detector.js
@@ -4,10 +4,10 @@ customElements.define('video-detector',
     welcome = null
     canvas = null
     alarm = null
-    W = 0
-    H = 0
     capture = null
     model = null
+    W = 0
+    H = 0
 
     async connectedCallback() {
       this.innerHTML = `
@@ -32,33 +32,33 @@ customElements.define('video-detector',
       this.H = this.canvas.height = img.height
       this.canvas = this.canvas.getContext('2d')
 
-      this.draw()
+      this.grab()
     }
 
-    async draw() {
+    async grab() {
       const img = await this.capture.grabFrame()
       this.canvas.drawImage(img, 0, 0)
       const predictions = await this.model.detect(this.canvas.getImageData(0, 0, this.W, this.H))
       const person = predictions.find(v => v.class === 'person')
       if (person !== undefined) {
-        this.drawAim(this.canvas, this.W/2, this.H/2)
+        const xy = person.bbox
+        this.drawAim(this.canvas, (xy[0]*2 + xy[2]) / 2, (xy[1]*2 + xy[3]) / 2)
         this.alarm.play()
       } else {
         this.alarm.pause()
         this.alarm.currentTime = 0
       }
-      window.requestAnimationFrame(this.draw.bind(this))
-/*    
+      window.requestAnimationFrame(this.grab.bind(this))
+      /*    
       const img2 = this.canvas.getImageData(0, 0, W, H);
       const pp = img2.data
-
       const ppl = pp.length / 4;
       for (let i = 0; i < ppl; i++) {
         const bw = (pp[i * 4 + 0] + pp[i * 4 + 1] + pp[i * 4 + 2]) / 3
         pp[i * 4 + 0] = pp[i * 4 + 1] = pp[i * 4 + 2] = bw
       }
       can2.putImageData(img2, 0, 0);
-*/
+      */
     }
 
     drawAim(can, x0, y0) {
@@ -66,17 +66,17 @@ customElements.define('video-detector',
       can.strokeStyle = 'rgba(255,255,255,0.5)'
 
       can.beginPath()
-      can.arc(x0, y0, 100, 0, Math.PI*2, false)
+      can.arc(x0, y0, 30, 0, Math.PI*2, false)
       can.stroke()
 
       can.beginPath()
-      can.moveTo(x0-30, y0)
-      can.lineTo(x0+30, y0)
+      can.moveTo(x0-10, y0)
+      can.lineTo(x0+10, y0)
       can.stroke()
 
       can.beginPath()
-      can.moveTo(x0, y0-30)
-      can.lineTo(x0, y0+30)
+      can.moveTo(x0, y0-10)
+      can.lineTo(x0, y0+10)
       can.stroke()
     }
   }
