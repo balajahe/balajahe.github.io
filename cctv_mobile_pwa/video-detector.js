07692b85692553941a7c8deacd392c21d89a6c98
balajahe <balajahe@gmail.com>
2020-03-05 17:45:07 +0300

    dev

--- cctv_mobile_pwa/video-detector.js
+++ cctv_mobile_pwa/video-detector.js
@@ -6,8 +6,10 @@ customElements.define('video-detector',
     canvas = null
     alarm = null
     stream = null
-    capture = null
     recorder = null
+    chunk = null
+    chunk_url = null
+    chunk_a = null
     W = 0
     H = 0
     stop = true
@@ -18,24 +20,23 @@ customElements.define('video-detector',
           <video autoplay muted></video>
           <canvas style="position:fixed; top:0; left:0"></canvas>
           <canvas style="display:none"></canvas>
-          <p style="display:none">
+          <div style="display:none">
             <button>Start detection</button>&emsp;
-            <button>Stop detection</button>
-          </p>
-          <div></div>
+            <button>Stop detection</button>&emsp;
+            <a target="_blank">Last video chunk</a>
+          </div>
           <audio loop src="./alarm.mp3"></audio>
       `
       this.video = this.querySelector('video')
       this.aim = this.querySelectorAll('canvas')[0]
       this.canvas = this.querySelectorAll('canvas')[1]
       this.alarm = this.querySelector('audio')
+      this.chunk_a = this.querySelector('a')
       this.querySelectorAll('button')[0].addEventListener('click', (_) => { 
-        this.recorder.start(5001)
         this.stop = false; 
         this.grab() 
       })
       this.querySelectorAll('button')[1].addEventListener('click', (_) => { 
-        this.recorder.stop()
         this.stop = true 
       })
 
@@ -45,29 +46,28 @@ customElements.define('video-detector',
 
       msg.innerHTML = 'Loading camera...'
       this.stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: {ideal: "environment"}}, audio: true})
+      const img = await (new ImageCapture(this.stream.getVideoTracks()[0])).grabFrame()
+      this.W = this.aim.width = this.canvas.width = img.width
+      this.H = this.aim.height = this.canvas.height = img.height
+      this.aim = this.aim.getContext('2d')
+      this.canvas = this.canvas.getContext('2d')
       this.video.srcObject = this.stream
 
       this.recorder = new MediaRecorder(this.stream, {mimeType : "video/webm"})
-      this.recorder.ondataavailable = (ev) => {
-        const blob = new Blob([ev.data], {type: "video/webm"})
-        const url = window.URL.createObjectURL(blob)
-        const a = document.createElement('a')
-        a.href = url
-        a.target = '_blank'
-        a.innerHTML = '# '
-        this.querySelector('div').appendChild(a)
-      }
-
-      this.capture = new ImageCapture(this.stream.getVideoTracks()[0])
-      const img = await this.capture.grabFrame()
-      this.W = this.aim.width = this.canvas.width = img.width
-      this.H = this.aim.height = this.canvas.height = img.height
-
-      this.aim = this.aim.getContext('2d')
-      this.canvas = this.canvas.getContext('2d')
+      this.recorder.addEventListener('dataavailable', (ev) => {
+        this.chunk = ev.data
+        URL.revokeObjectURL(this.chunk_url)
+        this.chunk_url = URL.createObjectURL(this.chunk)
+        this.chunk_a.href = this.chunk_url
+      })
+      this.recorder.start()
+      setInterval(() => {
+        this.recorder.stop()
+        this.recorder.start()
+      }, 5200)
 
       msg.remove()
-      this.querySelector('p').style.display = 'block'
+      this.querySelector('div').style.display = 'block'
     }
 
     async grab() {
@@ -77,8 +77,7 @@ customElements.define('video-detector',
         const person = predictions.find(v => v.class === 'person')
         if (person !== undefined) {
           if (this.recorder.state !== 'recording') this.recorder.start(5000)
-          const xy = person.bbox
-          this.draw_aim((xy[0]*2 + xy[2]) / 2, (xy[1]*2 + xy[3]) / 2)
+          this.draw(person.bbox)
           this.alarm.play()
         } else {
           this.stop_grab()
@@ -90,9 +89,19 @@ customElements.define('video-detector',
     }
 
     stop_grab() {
-      this.draw_aim(0, 0)
+      this.draw(null)
       this.alarm.pause()
       this.alarm.currentTime = 0
+    }
+
+    draw(b) {
+      const c = this.aim
+      c.clearRect(0, 0, this.W, this.H)
+      if (b !== null) {
+        c.lineWidth = 2;
+        c.strokeStyle = 'rgba(255,255,255,0.5)'
+        c.strokeRect(b[0], b[1], b[2], b[3])
+      }
     }
 
     draw_aim(x, y) {
