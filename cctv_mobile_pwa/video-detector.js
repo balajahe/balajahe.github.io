f12c4bfc10cbe44617f55b17b29a13abf1bc1897
balajahe <balajahe@gmail.com>
2020-03-05 12:29:47 +0300

    dev

--- cctv_mobile_pwa/video-detector.js
+++ cctv_mobile_pwa/video-detector.js
@@ -1,5 +1,6 @@
 customElements.define('video-detector',
   class extends HTMLElement {
+    model = null
     video = null
     aim = null
     canvas = null
@@ -7,37 +8,55 @@ customElements.define('video-detector',
     stream = null
     capture = null
     recorder = null
-    model = null
     W = 0
     H = 0
-    stop = false
+    stop = true
 
     async connectedCallback() {
       this.innerHTML = `
-          <p></p>
+          <p>Loading...</p>
+          <video autoplay muted></video>
+          <canvas style="position:fixed; top:0; left:0"></canvas>
+          <canvas style="display:none"></canvas>
           <p style="display:none">
             <button>Start detection</button>&emsp;
             <button>Stop detection</button>
           </p>
-          <video autoplay style="position:absolute"></video>
-          <canvas style="position:absolute"></canvas>
-          <canvas style="display:none"></canvas>
+          <div></div>
           <audio loop src="./alarm.mp3"></audio>
       `
       this.video = this.querySelector('video')
       this.aim = this.querySelectorAll('canvas')[0]
       this.canvas = this.querySelectorAll('canvas')[1]
       this.alarm = this.querySelector('audio')
-      this.querySelectorAll('button')[0].addEventListener('click', _ => { this.stop = false; this.grab() })
-      this.querySelectorAll('button')[1].addEventListener('click', _ => { this.stop = true })
+      this.querySelectorAll('button')[0].addEventListener('click', (_) => { 
+        this.recorder.start(5001)
+        this.stop = false; 
+        this.grab() 
+      })
+      this.querySelectorAll('button')[1].addEventListener('click', (_) => { 
+        this.recorder.stop()
+        this.stop = true 
+      })
 
-      const loading = this.querySelector('p')
-      loading.innerHTML = 'Loading neural network...'
+      const msg = this.querySelector('p')
+      msg.innerHTML = 'Loading neural network...'
       this.model = await cocoSsd.load()
 
-      loading.innerHTML = 'Loading camera...'
-      this.stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: {ideal: "environment"}}})
+      msg.innerHTML = 'Loading camera...'
+      this.stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: {ideal: "environment"}}, audio: true})
       this.video.srcObject = this.stream
+
+      this.recorder = new MediaRecorder(this.stream, {mimeType : "video/webm"})
+      this.recorder.ondataavailable = (ev) => {
+        const blob = new Blob([ev.data], {type: "video/webm"})
+        const url = window.URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.href = url
+        a.target = '_blank'
+        a.innerHTML = '# '
+        this.querySelector('div').appendChild(a)
+      }
 
       this.capture = new ImageCapture(this.stream.getVideoTracks()[0])
       const img = await this.capture.grabFrame()
@@ -47,7 +66,7 @@ customElements.define('video-detector',
       this.aim = this.aim.getContext('2d')
       this.canvas = this.canvas.getContext('2d')
 
-      loading.remove()
+      msg.remove()
       this.querySelector('p').style.display = 'block'
     }
 
@@ -57,20 +76,23 @@ customElements.define('video-detector',
         const predictions = await this.model.detect(this.canvas.getImageData(0, 0, this.W, this.H))
         const person = predictions.find(v => v.class === 'person')
         if (person !== undefined) {
+          if (this.recorder.state !== 'recording') this.recorder.start(5000)
           const xy = person.bbox
           this.draw_aim((xy[0]*2 + xy[2]) / 2, (xy[1]*2 + xy[3]) / 2)
           this.alarm.play()
         } else {
-          this.draw_aim(0, 0)
-          this.alarm.pause()
-          this.alarm.currentTime = 0
+          this.stop_grab()
         }
-          window.requestAnimationFrame(this.grab.bind(this))
-        } else {
-          this.draw_aim(0, 0)
-          this.alarm.pause()
-          this.alarm.currentTime = 0
-        }
+        window.requestAnimationFrame(this.grab.bind(this))
+      } else {
+        this.stop_grab()
+      }
+    }
+
+    stop_grab() {
+      this.draw_aim(0, 0)
+      this.alarm.pause()
+      this.alarm.currentTime = 0
     }
 
     draw_aim(x, y) {
