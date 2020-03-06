a52dccb5e421d62c632c96fab957f5c2da724817
balajahe <balajahe@gmail.com>
2020-03-06 11:14:58 +0300

    dev

--- cctv_mobile_pwa/video-detector.js
+++ cctv_mobile_pwa/video-detector.js
@@ -4,80 +4,82 @@ customElements.define('video-detector',
     video = null
     aim = null
     canvas = null
-    buttons = null
+    controls = null
     alarm = null
     stream = null
     recorder = null
-    chunk0 = null
+    chunk = null
     chunks = []
     W = 0
     H = 0
     detecting = false
     detected = false
-    recording = false
+    recording = 0
 
     async connectedCallback() {
       this.innerHTML = `
-          <p>Loading...</p>
-          <video autoplay muted></video>
+          <video autoplay muted style="display:none"></video>
           <canvas style="position:fixed; top:0; left:0"></canvas>
           <canvas style="display:none"></canvas>
+          <p>Loading...</p>
           <div style="display:none; width: 100vw">
-            <button>Start detection</button><br>
+            Email to send:
+            <input type="email"/>&nbsp;
+            <button>Start / Stop detection</button><br>
           </div>
           <audio loop src="./alarm.mp3"></audio>
       `
       this.video = this.querySelector('video')
       this.aim = this.querySelectorAll('canvas')[0]
       this.canvas = this.querySelectorAll('canvas')[1]
-      this.buttons = this.querySelector('div')
+      this.controls = this.querySelector('div')
       this.alarm = this.querySelector('audio')
       this.chunk_a = this.querySelector('a')
       this.querySelector('button').addEventListener('click', (ev) => { 
         if (!this.detecting) {
           this.detecting = true
-          this.grab()
-          ev.target.innerHTML = 'Stop detection'
+          this.grab_video()
         } else {
           this.detecting = false 
-          ev.target.innerHTML = 'Start detection'
         }
       })
 
       const msg = this.querySelector('p')
-      msg.innerHTML = 'Loading neural network...'
-      this.model = await cocoSsd.load()
-
       msg.innerHTML = 'Loading camera...'
       this.stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: {ideal: "environment"}}, audio: true})
-      const img = await (new ImageCapture(this.stream.getVideoTracks()[0])).grabFrame()
-      this.W = this.aim.width = this.canvas.width = img.width
-      this.H = this.aim.height = this.canvas.height = img.height
-      this.aim = this.aim.getContext('2d')
-      this.canvas = this.canvas.getContext('2d')
       this.video.srcObject = this.stream
+      this.video.addEventListener('loadedmetadata', (_) => {
+        this.W = this.aim.width = this.canvas.width = this.video.videoWidth
+        this.H = this.aim.height = this.canvas.height = this.video.videoHeight
+        this.aim = this.aim.getContext('2d')
+        this.canvas = this.canvas.getContext('2d')
+        this.video.style.display = 'block'
+      })
 
       this.recorder = new MediaRecorder(this.stream, {mimeType : "video/webm"})
       this.recorder.addEventListener('dataavailable', (ev) => {
+        this.chunk = ev.data
         if (this.detected) {
-          this.send(ev.data)
-        } else if (this.recording) {
-          this.send(ev.data)
-          this.recording = false
+          this.send_chunk()
+        } else if (this.recording > 0) {
+          this.send_chunk()
+          this.recording--
         }
-        this.chunk0 = ev.data
       })
       this.recorder.start()
       setInterval(() => {
         this.recorder.stop()
         this.recorder.start()
-      }, 5100)
+      }, 5500)
+
+      msg.innerHTML = 'Loading neural network...'
+      this.model = await cocoSsd.load()
 
       msg.remove()
-      this.querySelector('div').style.display = 'block'
+      this.controls.style.display = 'block'
     }
 
-    async grab() {
+    async grab_video() {
       if (this.detecting) {
         this.canvas.drawImage(this.video, 0, 0)
         const predictions = await this.model.detect(this.canvas.getImageData(0, 0, this.W, this.H))
@@ -85,16 +87,16 @@ customElements.define('video-detector',
         if (person !== undefined) {
           this.detected = true
           if (!this.recording) {
-            this.send(this.chunk0)
-            this.recording = true
+            this.send_chunk()
+            this.recording = 2
           }
-          this.draw(person.bbox)
+          this.draw_frame(person.bbox)
           this.alarm.play()
         } else {
           this.stop_alarm()
         }
-        //window.requestAnimationFrame(this.grab.bind(this))
-        setTimeout(this.grab.bind(this), 100)
+        //window.requestAnimationFrame(this.grab_video.bind(this))
+        setTimeout(this.grab_video.bind(this), 100)
       } else {
         this.stop_alarm()
       }
@@ -102,25 +104,26 @@ customElements.define('video-detector',
 
     stop_alarm() {
       this.detected = false
-      this.draw(null)
+      this.draw_frame(null)
       this.alarm.pause()
-      //this.alarm.currentTime = 0
+      this.alarm.currentTime = 0
     }
 
-    send(data) {
-      if (data !== null && data.size > 0) {
-        const url = URL.createObjectURL(data)
-        this.chunks.push({data, url})
+    send_chunk() {
+      if (this.chunk !== null && this.chunk.size > 0) {
+        this.chunks.push(this.chunk)
+        const i = this.chunks.length
+        const url = URL.createObjectURL(this.chunks[i-1])
         const a = document.createElement('a')
         a.href = url
         a.target = '_blank'
-        a.innerHTML = this.chunks.length
-        this.buttons.appendChild(document.createTextNode(' '))
-        this.buttons.appendChild(a)
+        a.innerHTML = ''+i
+        this.controls.appendChild(document.createTextNode(' '))
+        this.controls.appendChild(a)
       }
     }
 
-    draw(b) {
+    draw_frame(b) {
       const c = this.aim
       c.clearRect(0, 0, this.W, this.H)
       if (b !== null) {
