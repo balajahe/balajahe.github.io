4b2c1338bbd85b8574dc073f8ad9ecba92929cb1
balajahe <balajahe@gmail.com>
2020-03-17 16:32:43 +0300

    dev

--- cctv_nn/app-sw.js
+++ cctv_nn/app-sw.js
@@ -1,5 +1,5 @@
 const APP = 'cctv_nn'
-const VERSION = '.v4'
+const VERSION = '.v5'
 
 self.oninstall = (ev) => ev.waitUntil(
   caches.open(APP + VERSION)
--- cctv_nn/weird-component.js
+++ cctv_nn/weird-component.js
@@ -1,4 +1,4 @@
-export default class extends HTMLElement {
+export default class extends HTMLElement { 
     q(s, i) { 
         if (i === undefined)
             return this.querySelector(s)
