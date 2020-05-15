1ce8dadd4f380dd48a263a445bd5c98984dc4699
balajahe <balajahe@gmail.com>
2020-05-15 10:55:01 +0300

    dev

--- zdev/real_agent/page-new3-location.js
+++ zdev/real_agent/page-new3-location.js
@@ -6,8 +6,9 @@ customElements.define(me, class extends HTMLElement {
    connectedCallback() {
       this.innerHTML = `
          <style scoped>
+            ${me} iframe { width: 100%; }
          </style>
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d575869.7969494411!2d39.58731392946736!3d44.004863867825826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f69a97320fb1c3%3A0x4d8520015f1ad2f3!2z0YDRg9GHLiDQktC-0LTQvtC_0LDQtNC40YHRgtGL0Lk!5e0!3m2!1sru!2sru!4v1589474666577!5m2!1sru!2sru" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"></iframe>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4166.955461487204!2d39.867116297673405!3d43.9442505414373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f684d9d934eca5%3A0x85ac36acd4cd77a9!2z0KfQuNCz0YPRgNGB0LDQvdCwLCDQoNC10YHQv9GD0LHQu9C40LrQsCDQkNC00YvQs9C10Y8sIDM4NTc5Nw!5e1!3m2!1sru!2sru!4v1589529205815!5m2!1sru!2sru" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       `
       wcMixin(this)
    }
