34584ecedc03b64e4ca4cb1c86ffc026ffed23e6
balajahe <balajahe@gmail.com>
2020-05-30 06:04:17 +0300

    dev

--- real_agent/obj-edit.js
+++ real_agent/obj-edit.js
@@ -92,14 +92,12 @@ customElements.define(me, class extends HTMLElement {
 		const draw = () => this.mapIframe.contentWindow.location.replace(`https://www.openstreetmap.org/export/embed.html?bbox=${this.location.longitude-0.002}%2C${this.location.latitude-0.002}%2C${this.location.longitude+0.002}%2C${this.location.latitude+0.002}&layer=mapnik&marker=${this.location.latitude}%2C${this.location.longitude}`)
 	
 		if (this.obj.location.latitude && this.obj.location.longitude) {
-			this.location.latitude = this.obj.location.latitude
-			this.location.longitude = this.obj.location.longitude 
+			this.location = { latitude: this.obj.location.latitude, longitude: this.obj.location.longitude }
 			draw()
 	
 		} else {
 			navigator.geolocation.getCurrentPosition(loc => {
-				this.location.longitude = loc.coords.longitude
-				this.location.latitude = loc.coords.latitude
+				this.location = { latitude: loc.coords.latitude, longitude: loc.coords.longitude }
 				draw()
 				APP.message('LOCATION UPDATED!')
 			})
