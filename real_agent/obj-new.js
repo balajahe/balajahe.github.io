1f82b7ed1cb6d896bb35fffca72f3c6a65e19c95
balajahe <balajahe@gmail.com>
2020-06-06 05:49:58 +0300

    dev

--- real_agent/obj-new.js
+++ real_agent/obj-new.js
@@ -73,10 +73,10 @@ customElements.define(me, class extends HTMLElement {
 				this.location = { latitude: loc.coords.latitude, longitude: loc.coords.longitude }
 				this.mapIframe.contentWindow.location.replace(`https://www.openstreetmap.org/export/embed.html?bbox=${this.location.longitude-0.002}%2C${this.location.latitude-0.002}%2C${this.location.longitude+0.002}%2C${this.location.latitude+0.002}&layer=mapnik&marker=${this.location.latitude}%2C${this.location.longitude}`)
 			},
-			null,
+			err => alert(err),
 			{
 			  enableHighAccuracy: true,
-			  timeout: 0,
+			  timeout: 10000,
 			  maximumAge: 0
 			}
 		)
