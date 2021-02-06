f51f78a4f5db876a227b2df2aaeda2df5e98c1c8
Balajahe <balajahe@gmail.com>
2021-02-06 22:02:18 +0300

    dev

--- AbandonedRussia/android/app/src/main/kotlin/com/example/AbandonedRussia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.balajahe.AbandonedRussia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- zdev/flutter/RandomPersons/README.md
+++
@@ -1,3 +0,0 @@
-# RandomPersons
-
-Infinite random person list wiht error handling
