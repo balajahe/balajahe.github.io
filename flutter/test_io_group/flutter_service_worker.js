8206c398e973878a1d51ce2d9fe7931f602e6541
Balajahe <balajahe@gmail.com>
2021-02-08 20:43:00 +0300

    dev

--- flutter/test_io_group/flutter_service_worker.js
+++ flutter/test_io_group/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "002301040a2d706d402fd285195cfe6f",
-"index.html": "ca167ee9201774655b01f86918397501",
-"/": "ca167ee9201774655b01f86918397501",
+"index.html": "e4d2f1f52457e20134cc6a1cedd00b66",
+"/": "e4d2f1f52457e20134cc6a1cedd00b66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
