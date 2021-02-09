f85905a12cc0ade991020aa4203cb693632cae4e
Balajahe <balajahe@gmail.com>
2021-02-09 13:25:51 +0300

    dev

--- flutter/test_io_group/flutter_service_worker.js
+++ flutter/test_io_group/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "002301040a2d706d402fd285195cfe6f",
-"index.html": "92668ceab6959f96cc85083effae24d9",
-"/": "92668ceab6959f96cc85083effae24d9",
+"index.html": "51439134f1c118461b6437c042c53dff",
+"/": "51439134f1c118461b6437c042c53dff",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "assets/assets/blank_photo.png": "37e779c64e53dff32b3d7fe21f94de46",
 "assets/NOTICES": "965f3055abf6fcf53cd10b4739e70676",
 "assets/AssetManifest.json": "6c4a3d2cabaf53724d011c9ed1e97822",
-"main.dart.js": "de8c1ffe0b1de79afd5efc9e597b8d68",
+"main.dart.js": "a46e6d0e567e402d591309068f7a736c",
 "manifest.json": "a5b3efec562705bb9cb7c92924a1c614"
 };
 
