eb82a9c9c03214b551d9d231f1e94dd8c9bd94fd
Balajahe <balajahe@gmail.com>
2021-03-11 15:31:56 +0300

    dev

--- flutter/mmk_lite/flutter_service_worker.js
+++ flutter/mmk_lite/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c0a195b4ed2da38979bae4b17be6895d",
-"index.html": "dd53eeb1d366b64d7a3b3723a561ab09",
-"/": "dd53eeb1d366b64d7a3b3723a561ab09",
+"index.html": "e948c817a395685f26b05975bec9d713",
+"/": "e948c817a395685f26b05975bec9d713",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
@@ -15,9 +15,9 @@ const RESOURCES = {
 "assets/assets/blank_avatar.png": "ffbf271a1fb98ea6316e1d3031dfa889",
 "assets/assets/anon_avatar.png": "96ba6480ecb536c07185a4f2caf43bb9",
 "assets/assets/mmk_logo.png": "5406cbbf7e91978ac303edee68b8bf09",
-"assets/NOTICES": "1734b5e9912fe80e5e45df40fe45ac7e",
+"assets/NOTICES": "8dc86de6c58736df5ee1a0cccb600ca6",
 "assets/AssetManifest.json": "dce60ef0f913c579269bb120bd288fbb",
-"main.dart.js": "ef0679c4f0082387509f384636e76bd4",
+"main.dart.js": "f0a796e5c52ac0182d708a1b3c3b2507",
 "manifest.json": "c32c899d2e6f3412ab5de4dcbc395f02"
 };
 
