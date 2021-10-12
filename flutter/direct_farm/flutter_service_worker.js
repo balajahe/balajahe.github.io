9452cd2c8fd4ce5f1c933df843f27162cc55b9fa
Balajahe <balajahe@gmail.com>
2021-10-12 09:26:24 +0300

    dev

--- flutter/direct_farm/flutter_service_worker.js
+++ flutter/direct_farm/flutter_service_worker.js
@@ -9,8 +9,8 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "27c914fedf840e0267e5694b40b19793",
-"/": "27c914fedf840e0267e5694b40b19793",
+"index.html": "e698d6e5ca303722d195f0547f5b2a89",
+"/": "e698d6e5ca303722d195f0547f5b2a89",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/country_code_picker/i18n/mn.json": "6f69ca7a6a08753da82cb8437f39e9a9",
@@ -348,7 +348,7 @@ const RESOURCES = {
 "assets/assets/login-apple.png": "5681cf3451575b501bf285733e5a9b9a",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "64781841791d792b389f7a2f3440105e",
-"main.dart.js": "572f195e90280ef0161519ad15eb663c",
+"main.dart.js": "547bafc5ca77aeb95bafb7fd5896614c",
 "manifest.json": "014c15ef7e64e9b9d669c6faa09041d7"
 };
 
